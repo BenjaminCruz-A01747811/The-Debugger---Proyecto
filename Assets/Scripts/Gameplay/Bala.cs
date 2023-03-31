c0dd9519fb9edf9b156f181fde68768d6091173c
BenjaminCruz-A01747811 <95510233+BenjaminCruz-A01747811@users.noreply.github.com>
2023-03-31 15:05:37 -0600

    Cambio en códigos y prefabs (nuevo Explosión)

--- Assets/Scripts/Gameplay/Bala.cs
+++ Assets/Scripts/Gameplay/Bala.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Bala : MonoBehaviour
 {
     [SerializeField] private float velocidad;
+
     [SerializeField] private float daño;
 
     private void Update()
     {
-        transform.Translate(Vector2.up * velocidad * Time.deltaTime);
+        transform.Translate(Vector2.right * velocidad * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
